cf9f34a7c25ed522e64ee7eac52056a99078a44f
Curtis Jochen <curtis.jochen@gmail.com>
2019-07-05 14:32:26 -0400

    Begin setting up HTML framework as well as calling google API to the page. Added a Style Reset and some css styling. This is just a starting point not complete

---
+++ assets/javascript/app.js
@@ -0,0 +1,67 @@
+var map;
+function initMap() {
+ map = new google.maps.Map(document.getElementById('map'), {
+   center: {lat: -34.397, lng: 150.644},
+   zoom: 8
+ });
+}
+
+initMap();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Google Maps API key - Curtis
+//AIzaSyCuRk6NNaEEnPYbqhunY4xCs96MvlNoPIw
+
+//Storm Glass API call example
+/*const lat = 36.4124;
+const lng = 74.0319;
+const params = 'seaLevel';
+
+fetch(`https://api.stormglass.io/v1/weather/point?lat=${lat}&lng=${lng}&params=${params}`, {
+  headers: {
+    'Authorization': '11ed69c2-9d29-11e9-9e74-0242ac130004-11ed6ab2-9d29-11e9-9e74-0242ac130004'
+  }
+}).then((response) => response.json()).then((jsonData) => {
+  console.log(jsonData);
+});*/
