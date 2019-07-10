4d16ff5d9e9e6e688b0c969e8bcc8d429f2da157
Curtis Jochen <curtis.jochen@gmail.com>
2019-07-09 20:43:24 -0400

    Attempting to overwrite the master file with a much cleaner working local file. Previous master file had many errors due to two merges not working as expected

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,9 +9,8 @@ function initMap() {
 
 initMap();
 
-
 function currentWeather() {
-  var key = '184eb6450fef976a167dbae5ef669d22';
+  let key = '184eb6450fef976a167dbae5ef669d22';
   fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.5241&lon=-77.4223&appid=' + key)
     .then(function (resp) { return resp.json() }) // Convert data to json
     .then(function (data) {
@@ -22,29 +21,13 @@ function currentWeather() {
     });
 }
 
-
-// function weatherRadar() {
-//   var key = '184eb6450fef976a167dbae5ef669d22';
-//   fetch('http://maps.openweathermap.org/maps/2.0/weather/TA2/{12}/{37.5241}/{-77.4223}?date=000107092019&opacity=0.9&fill_bound=true&palette=0:FF0000;10:00FF00;20:0000FF&appid=' + key)
-//     .then(function (resp) { return resp.json() }) // Convert data to json
-//     .then(function (data) {
-//       console.log(data);
-//     })
-//     .catch(function () {
-//       // catch any errors
-//     });
-// }
-
-
 window.onload = function () {
   currentWeather();
-  // weatherRadar();
 }
 
-
 function drawWeather(d) {
-  // var celcius = Math.round(parseFloat(d.main.temp)-273.15);
-  var fahrenheit = Math.round(((parseFloat(d.main.temp) - 273.15) * 1.8) + 32);
+  // let celcius = Math.round(parseFloat(d.main.temp)-273.15);
+  let fahrenheit = Math.round(((parseFloat(d.main.temp) - 273.15) * 1.8) + 32);
 
   document.getElementById('description').innerHTML = titleCase(d.weather[0].description);
   document.getElementById('temp').innerHTML = fahrenheit + '&deg;';
@@ -59,54 +42,43 @@ function titleCase(string) {
   return string
 }
 
-var radar = L.map('radar', {
-  center: [37.5241, -77.4223],
-  zoom: 12,
-  minZoom: 9,
-  maxZoom: 14,
-  zoomControl: false,
-})
+console.log(">hello");
 
-var key = '184eb6450fef976a167dbae5ef669d22';
-var radarTemp = L.tileLayer('http://maps.openweathermap.org/maps/2.0/weather/TA2/{12}/{37.5241}/{-77.4223}?date=010007092019&opacity=0.8&fill_bound=true&appid' + key,{
-minZoom: 9,
-maxZoom: 14
+let zoom = 5;
+let radar = L.map('radar').setView([37.5241, -77.4223], zoom);
+
+let mapboxKey = 'pk.eyJ1Ijoia3lsZWNveDIxMyIsImEiOiJjanh3Z2liNnQwMzQ1M2xvemRuM2RvcWRqIn0.Q5gET69Q50YjzXLj1zRp7g';
+L.tileLayer(`https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=${mapboxKey}`, {
+	minZoom: 0,
+	maxZoom: 18,
+	//z: zoom,
+	id: 'mapbox.streets'
 }).addTo(radar);
 
+let rows = Math.pow(2, zoom);
+let cols = rows;
+
+let key = '184eb6450fef976a167dbae5ef669d22';
+let layer = `temp_new`;
+let z = zoom;
+let x = Math.floor(rows / 2);
+let y = Math.floor(cols / 2);
+let url = `https://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?appid={api_key}`;
+console.log(url);
+let radarTemp = L.tileLayer(url, {
+	layer: layer,
+	api_key: key,
+	//z: z,
+	//x: x,
+	//y: y,
+	minZoom: 0,
+	maxZoom: 18}
+	).addTo(radar);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // function drawTemp(d){
-// //     document.getElementById('radar')
-// // }
-
-
-// //Google Maps API key - Curtis
-// //AIzaSyCuRk6NNaEEnPYbqhunY4xCs96MvlNoPIw
-
-// //Storm Glass API call example
-// /*const lat = 36.4124;
-// const lng = 74.0319;
-// const params = 'seaLevel';
-
-// fetch(`https://api.stormglass.io/v1/weather/point?lat=${lat}&lng=${lng}&params=${params}`, {
-//   headers: {
-//     'Authorization': '11ed69c2-9d29-11e9-9e74-0242ac130004-11ed6ab2-9d29-11e9-9e74-0242ac130004'
-//   }
-// }).then((response) => response.json()).then((jsonData) => {
-//   console.log(jsonData);
-// });*/
+	// Each map tile is a 256x256 point square. At zoom level 0, the entire world is 
+	// rendered in a single tile. Each zoom level increases the magnification by a 
+	// factor of two. So, at zoom level 1 the map will be rendered as a 2x2 grid of 
+	// tiles, or a 4x4 grid at zoom level 2, a 8x8 grid at zoom level 3, and so on. If 
+	// you are creating images for a tile layer, you will need to create a new 256x256 
+	// point image for each tile at each zoom level that you wish to support.
